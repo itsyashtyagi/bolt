b612c66de5c2005cc39bb967468a92a3fb6676ed
yashtyagi <yashtyagi8006@gmail.com>
2026-07-06 23:38:09 +0530

    backend code

--- be/src/index.ts
+++ be/src/index.ts
@@ -1,24 +1,72 @@
 import Anthropic from '@anthropic-ai/sdk';
 import dotenv from "dotenv";
+import express from "express";
+import { BASE_PROMPT } from "./prompts.js";
+import { basePrompt as nodeBasePrompt } from "./defaults/node.js";
+import { basePrompt as reactBasePrompt } from './defaults/react.js';
+import type { TextBlock } from '@anthropic-ai/sdk/resources';
+import cors from "cors";
+import { getSystemPrompt } from './prompts.js';
 
 dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
 
 const anthropic = new Anthropic();
 
-const stream = anthropic.messages
-  .stream({
+app.post("/template", async (req, res) => {
+  const prompt = req.body.prompt;
+
+  const response = await anthropic.messages.create({
     model: "claude-opus-4-8",
     max_tokens: 1024,
     messages: [
       {
         role: "user",
-        content: "Say hello there!"
+        content: prompt
       }
-    ]
-  })
-  .on("text", (text) => {
-    console.log(text);
+    ], 
+    system: "Return either node or react based on what do you think this project should be. Only return a single word either 'node' or 'react'. Do not return anything extra"
   });
 
-const message = await stream.finalMessage();
-console.log(message);
+  const answer = (response.content[0] as TextBlock).text;
+  if(answer === "react"){
+    res.json({
+      prompts: [BASE_PROMPT, `Here is an artifact that contains all files of the project visible to you.\nConsider the contents of ALL files in the project.\n\n${reactBasePrompt}\n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n  - .gitignore\n  - package-lock.json\n`],
+      uiPrompts: [reactBasePrompt]
+    });
+    return;
+  }
+
+  if(answer === "node"){
+    res.json({
+      prompts: [`Here is an artifact that contains all files of the project visible to you.\nConsider the contents of ALL files in the project.\n\n${nodeBasePrompt}\n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n  - .gitignore\n  - package-lock.json\n`],
+      uiPrompts: [nodeBasePrompt]
+    });
+    return;
+  }
+
+  res.status(403).json({
+    message : "You cannot access this"
+  })
+  return;
+});
+
+app.post("/chat", async (req, res) => {
+  const messages = req.body.messages;
+  const response = await anthropic.messages.create({
+    messages: messages, 
+    model: "claude-opus-4-8",
+    max_tokens: 8000,
+    system: getSystemPrompt()
+  })
+
+  console.log(response);
+
+  res.json({
+    response: (response.content[0] as TextBlock)?.text
+  });
+});
+
+app.listen(3000);
